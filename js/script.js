f89db327427fd388bad89f359e01c31c422db965
Manideep Nakka <117279997+Manideep4005@users.noreply.github.com>
2024-07-20 00:14:51 +0530

    Update script.js
    Signed-off-by: Manideep Nakka <117279997+Manideep4005@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,50 +1,5 @@
-const header = document.getElementById('header');
-const openbtn = document.getElementById('open-btn');
-const showbtns = document.getElementById('head-links');
-const closebtn = document.getElementById('close-btn');
-const jumpbtns = document.querySelectorAll('#jump-btn');
-
-openbtn.addEventListener('click', () => {
-  showbtns.style.width = '300px';
-  document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-  document.querySelector('.overlay').style.display = 'block';
-  document.getElementById('myBtn').style.zIndex = 5;
-});
-
-closebtn.addEventListener('click', () => {
-  showbtns.style.width = '0';
-  document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
-  document.querySelector('.overlay').style.display = 'none';
-});
-
-function closeModal() {
-  showbtns.style.width = '0';
-  document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
-  document.querySelector('.overlay').style.display = 'none';
-}
-
-for (let i = 0; i < jumpbtns.length; i++)
-  jumpbtns[i].addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  console.log(e.key);
-
-  // check the key is 'Escape'
-  if (e.key === 'Escape') {
-    showbtns.style.width = '0';
-    document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
-    document.querySelector('.overlay').style.display = 'none';
-  }
-});
-
-document.querySelector('.overlay').addEventListener('click', () => {
-  showbtns.style.width = '0';
-  document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
-  document.querySelector('.overlay').style.display = 'none';
-});
-
 // go to top button
-let myBtn = document.getElementById('myBtn');
+let myBtn = document.querySelector('.top-btn');
 
 window.onscroll = function () {
   scrollFunction();
@@ -52,16 +7,98 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 500 ||
-    document.documentElement.scrollTop > 500
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
   ) {
-    myBtn.style.display = 'block';
+    myBtn.style.display = 'flex';
   } else {
     myBtn.style.display = 'none';
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 75;
+  document.documentElement.scrollTop = 75;
 }
+
+// FUNCTION FOR OPENING MY IMAGE MODAL
+const openImgBtn = document.querySelectorAll('#my-img');
+const closeImgBtn = document.querySelector('#close-img-btn');
+const imgModal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+function openImg() {
+  imgModal.style.display = 'flex';
+  document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
+  document.querySelector('.overlay').style.display = 'block';
+  console.log('btn is clicked');
+}
+
+function closeImg() {
+  imgModal.style.display = 'none';
+  document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
+  document.querySelector('.overlay').style.display = 'none';
+  console.log('btn is clicked');
+}
+
+for (let j = 0; j < openImgBtn.length; j++)
+  openImgBtn[j].addEventListener('click', openImg);
+
+closeImgBtn.addEventListener('click', closeImg);
+
+const root = document.querySelector(':root');
+
+const dark = document.querySelectorAll('#dark');
+
+const header = document.querySelector('.header-1');
+
+function toggleDark() {
+  if (root.classList.contains('dark')) {
+    root.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  } else {
+    root.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
+if (localStorage.getItem('theme') === 'dark') {
+  root.classList.add('dark');
+}
+
+for (let i = 0; i < dark.length; i++)
+  dark[i].addEventListener('click', toggleDark);
+
+/* Side Bar Open */
+const sidebarOpenBtn = document.getElementById('open-sidebar');
+const sideBarCloseBtn = document.getElementById('sidebar-close');
+const sidebar = document.querySelector('.sidebar');
+const sidebarBtns = document.querySelectorAll('#sidebar-btns');
+
+function openSidebar() {
+  sidebar.style.width = '300px';
+  document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
+  document.querySelector('.overlay').style.display = 'block';
+}
+
+function closeSideBar() {
+  sidebar.style.width = '0';
+  document.getElementsByTagName('body')[0].style.overflowY = 'scroll';
+  document.querySelector('.overlay').style.display = 'none';
+}
+
+sidebarOpenBtn.addEventListener('click', openSidebar);
+sideBarCloseBtn.addEventListener('click', closeSideBar);
+
+for (let i = 0; i < sidebarBtns.length; i++)
+  sidebarBtns[i].addEventListener('click', closeSideBar);
+
+overlay.addEventListener('click', closeImg);
+overlay.addEventListener('click', closeSideBar);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeImg();
+    closeSideBar();
+  }
+});
